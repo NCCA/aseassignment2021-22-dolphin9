769cd6634aa955fd005234f3c64db3969d16ba99
dolphin9 <dolphin9chy@gmail.com>
2024-01-21 05:42:14 +0000

    change idea

---
+++ test.py
@@ -0,0 +1,13 @@
+import json
+import os
+import time
+import subprocess
+import threading
+import numpy as np
+import pyautogui
+import pydirectinput
+import math
+import cv2
+
+pydirectinput.moveTo(100, 150)
+pydirectinput.click()
