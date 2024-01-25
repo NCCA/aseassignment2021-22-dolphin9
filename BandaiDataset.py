70eae6961888632275ac23e604d8fef1acf90e3f
dolphin9 <dolphin9@foxmail.com>
2024-01-25 06:38:20 +0000

    add some note in BandaiDataset.py and file  train.ipynb

--- BandaiDataset.py
+++ BandaiDataset.py
@@ -2,16 +2,24 @@ import os
 import torch
 import cv2 as cv
 import numpy as np
-import pandas as pd
 import json
 import copy
 from matplotlib import pyplot as plt
-import torchvision.transforms as transforms
 from torch.utils.data import Dataset
-from PIL import Image
+
+"""
+There are two classes are contained in the file
+Motion class is for the motion data, which is the dataframe for BandaiDataset
+
+"""
+
+VIDEO_DIR = 'datasets/mp4/'
+JSON_DIR = 'datasets/data/'
+LABEL_DIR = 'datasets/cfg/'
+LIST_FILE = 'datafiles.txt'
+
 
 class Motion:
-
 
     def __init__(self):
         self.pose_list = []
@@ -20,12 +28,16 @@ class Motion:
         
 
     # aquire pose images from vedio using OpenCV
-    def input_motion(self,video_dir,filename, json_dir = '',):
+    def input_motion(self, video_dir:str, filename:str, json_dir = '',):
+        """
+        * notice : filename dosen't contain its suffix 
+        This function is to read .mp4 files by OpenCV.VideoCaputre
+        """
         
         if filename == '':
             return False
                
-        self.read_label(json_dir+filename+".json")
+        self.input_label(json_dir+filename+".json")
 
         cap = cv.VideoCapture(video_dir + filename +'.mp4')
         self.pose_list = []
@@ -43,28 +55,34 @@ class Motion:
 
         return True
     
-    def get_pose(self, i):
+    def get_pose(self, i:int) ->list:
         return self.pose_list[i]
     
-    # draw certain pose in motion
-    def draw_pose(self, i):
+    def draw_pose(self, i:int) -> None:
         plt.imshow(self.pose_list[i],cmap='gray')
 
-    def read_label(self,label_path):
+    def input_label(self,label_path:str) -> None:
+
         with open(label_path) as f:
             jsondata = json.loads(f.readline())
         self.label = jsondata['content']
 
-    ##### here , transfer motions in to same frames.
-    def adjust(self, frame_lenth):  
+    def adjust(self, frame_lenth: int) -> None :
+        """
+            tansfer motion into certain frames
+            Two strategies are used in this funcion
+                1. for the motion which the number of frame is less than we expect
+                    copy the whole sequence in the end 
+                2. for the those which the number of frame is more than we expect
+                    remain the mid part as they are more representative 
+                 
+        """
         if len(self.pose_list) == frame_lenth:
             return
 
         new_pose_list = copy.deepcopy(self.pose_list)
-        less_flag = False
         if len(new_pose_list)< frame_lenth:
             new_pose_list.extend(copy.deepcopy(self.pose_list))
-            less_flag = True
             while len(new_pose_list) < frame_lenth:
                 new_pose_list.extend(copy.deepcopy(self.pose_list))
         
@@ -74,8 +92,6 @@ class Motion:
             end_frame = start_frame + frame_lenth
             new_pose_list = copy.deepcopy(new_pose_list[start_frame:end_frame])
 
-        #print(len(new_pose_list))
-        #print(frame_lenth)
         if frame_lenth == len(new_pose_list):
             self.pose_list = copy.deepcopy(new_pose_list)
             self.frame_num = frame_lenth
@@ -83,19 +99,19 @@ class Motion:
             print('Err: len(new_pose_lise) is not equal to frame lenth!')
 
 
+    def get_motion_tensor(self, set_frame:int) -> torch.Tensor:
 
-    
-    def get_motion_tensor(self, set_frame):
         if self.frame_num != set_frame:
             self.adjust(frame_lenth=set_frame)
 
-
-        ######################
         motion = np.array(self.pose_list)
         motion_tensor = torch.from_numpy(motion)
         return motion_tensor
     
-    def write_vedio(self,filename):
+    def write_vedio(self,filename:str):
+        '''
+            to be fixed!
+        ''' 
         fourcc = cv.VideoWriter_fourcc('M','P','4','V')
         out = cv.VideoWriter(filename, fourcc, 20.0, (640,480))
         for i in range(0,self.frame_num):
@@ -107,25 +123,31 @@ class Motion:
     
 
 class BandaiDataset(Dataset):
-    
-    filelist = []
+    """
+        BandaiDataset inherits from torch.utils.data.Dataset
+    """
+
     motion_list = []
+
+    filelist = []
     label_list = []
+
     num_of_files = 0
     max_frame = -1
     min_frame = 10000
-    video_dir = 'datasets/mp4/'
-    json_dir = 'datasets/data/'
-    label_dir = 'datasets/cfg/'
-    list_file = 'datafiles.txt'
 
-    def __init__(self, filepath = ''):
+
+    def __init__(self, filepath:str = LIST_FILE):
+        """
+            a filepath should be specfied to save the list of filename for data
+            defult filepath is 'datafiles.txt' in the root directory
+        """
         super().__init__()
 
         if filepath == '':
-            filepath = self.list_file
+            filepath = LIST_FILE
         else:
-            self.list_file = filepath
+            LIST_FILE = filepath
 
         self.get_filenames()
         
@@ -136,23 +158,24 @@ class BandaiDataset(Dataset):
     def __getitem__(self, i):
         return self.motion_list[i]
     
-    def load(self):
-        self.get_filenames(self.list_file)
+    def load(self, list_file:str = LIST_FILE):
+        """
+            load data 
+        """
 
-        # count the number of files as cap frames from vedios
+        self.get_filenames(LIST_FILE)
         for filename in self.filelist:
-            #
             motion = Motion()
             flag = motion.input_motion(video_dir=self.video_dir,json_dir=self.json_dir,filename=filename)
             if flag:
                 self.motion_list.append(copy.deepcopy(motion))
-                #print(self.motion_list[-1].frame_num)
             
 
     
-    def get_filenames(self, filepath=''):
-        if filepath == '':
-            filepath = self.list_file
+    def get_filenames(self, filepath:str = LIST_FILE) -> list:
+
+        if filepath == '' or filepath == LIST_FILE:
+            pass
         else:
             self.filelist = []
 
@@ -172,6 +195,9 @@ class BandaiDataset(Dataset):
         return self.filelist
     
     def preprocessing(self):
+        '''
+            is dropped
+        '''
         for motion in self.motion_list:
             self.max_frame = max(motion.frame_num,self.max_frame)
             self.min_frame = min(motion.frame_num,self.min_frame)
---
+++ mymodel.py
@@ -0,0 +1,249 @@
+import BandaiDataset as bd
+import numpy as np
+import torch    
+import torch.nn as nn
+import multiprocessing as mp
+from torchsummary import summary
+from torch.utils.data import DataLoader, random_split
+
+set_frame = 50
+epochs = 60
+batch_sz = 10
+checkpoint_frequency = 3
+learning_rate = 0.00005
+gamma = 0.5
+
+SAVE_DIR = './save_models'
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class ConvNet(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=50, out_channels=50, kernel_size=5, stride=1, padding=2) #[(dimension_sz−kernel+2*Padding)/Stride]+1
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+
+        self.conv2 = nn.Conv2d(in_channels=50, out_channels=50, kernel_size=5, stride=1, padding=2) #[(dimension_sz−kernel+2*Padding)/Stride]+1
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2)  
+        '''
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=8, kernel_size=5, stride=1, padding=2) #[(dimension_sz−kernel+2*Padding)/Stride]+1
+        self.relu3 = nn.ReLU()
+        #self.pool3 = nn.MaxPool2d(kernel_size=2)  
+        ''' 
+        self.flat = nn.Flatten()            
+        self.fc1 = nn.Linear(50 * 160 * 120, 128)   
+        self.relu4 = nn.ReLU()
+
+        self.fc2 = nn.Linear(128, 256)
+        self.relu5 = nn.ReLU()
+
+        self.fc3 = nn.Linear(256, 64)
+        self.relu6 = nn.ReLU()
+
+        self.fc4 = nn.Linear(64, 10)
+
+    def forward(self, inp):
+        inp = self.conv1(inp)
+        inp = self.relu1(inp) 
+        inp = self.pool1(inp)
+
+        inp = self.conv2(inp)
+        inp = self.relu2(inp) 
+        inp = self.pool2(inp)
+        '''
+        inp = self.conv3(inp)
+        inp = self.relu3(inp) 
+        #inp = self.pool3(inp)       
+        ''' 
+        inp = self.flat(inp)
+
+        inp = self.fc1 (inp) 
+        inp = self.relu4(inp)            
+        inp = self.fc2(inp) 
+        inp = self.relu5(inp)
+        inp = self.fc3(inp) 
+        inp = self.relu6(inp)
+        out = self.fc4(inp)
+        return out
+    
+
+def get_simple_conv_net():
+    return ConvNet()
+
+def show_net_summary():
+    summary(get_simple_conv_net(), input_size=(50, 640, 480), device="cpu")
+
+
+def custom_collate_fn(batch):
+    motion_batch_tensor = torch.FloatTensor(len(batch),50,480,640)
+    motion_tensors = []
+    labels = []
+    #print(type(batch))
+
+    for item in batch:
+        #print(item)
+        motion_tensor = item.get_motion_tensor(50) # load an motion as a tensor(frames,width,height)
+        motion_tensors.append(motion_tensor.unsqueeze(0)) # put motions into a list : to be checked 
+        labels.append(item.label)
+
+    torch.cat(motion_tensors, out=motion_batch_tensor)
+    label_batch_tensor = torch.LongTensor(labels)
+    return (motion_batch_tensor,label_batch_tensor)
+
+def load_data(file_list_path= '', data_path='', batch_sz = 5, train_val_test_split = [0.7,0.1,0.2]):
+    assert sum(train_val_test_split) == 1, "Train, val and test fractions should sum to 1!" 
+    dataset = bd.BandaiDataset(data_path)
+    dataset.load()
+
+    tr_va_te = []
+    n_cpus = mp.cpu_count()
+    
+    for frac in train_val_test_split:
+        num = round(frac * dataset.num_of_files)
+        tr_va_te.append(num)
+    
+    if tr_va_te[0] != (dataset.num_of_files - tr_va_te[1] - tr_va_te[2]):
+        tr_va_te[0] = (dataset.num_of_files - tr_va_te[1] - tr_va_te[2])
+
+    train_split, val_split, test_split = random_split(dataset, tr_va_te)
+
+    train_dl = DataLoader(train_split,
+                          batch_size=batch_sz,
+                          shuffle=True,
+                          collate_fn=custom_collate_fn,
+                          num_workers=n_cpus
+                        )
+    val_dl = DataLoader(val_split,
+                        batch_size=batch_sz,
+                        shuffle=True,
+                        collate_fn=custom_collate_fn,
+                        num_workers=n_cpus)
+    test_dl = DataLoader(test_split,
+                         batch_size=batch_sz,
+                         shuffle=True,
+                         collate_fn=custom_collate_fn,
+                         num_workers=n_cpus)
+
+    return train_dl, val_dl, test_dl
+
+
+class EarlyStopper:
+    def __init__(self, patience =  1, tolerance = 0):
+        self.patience = patience
+        self.tolerance = tolerance
+
+        self.epoch_counter = 0
+        self.max_validation_acc = np.NINF
+
+    def should_stop(self, validation_acc):
+        if validation_acc > self.max_validation_acc:
+            self.max_validation_acc = validation_acc
+            self.epoch_counter = 0
+        elif validation_acc < (self.max_validation_acc - self.tolerance):
+            self.epoch_counter += 1
+            if(self.epoch_counter >= self.patience):
+                return True
+        return False
+    
+def save_checkpoint(model, epoch, save_dir = SAVE_DIR):
+    filename = f"checkout_{epoch}.pth"
+    save_path = f"{save_dir}/{filename}"
+    torch.save(model.state_dict(), save_path)
+
+    motion = bd.Motion()
+    motion.adjust()
+
+def train_model_final(model, epochs, dataloaders,
+                      optimizer, lr_scheduler, writer,
+                      early_stopper,checkpoint_frequency):
+    msg = ""
+    for epoch in range(epochs):
+        ################# TRAINING ####################
+        model.train()
+        train_dl = dataloaders['train']
+
+        total_steps_train = len(train_dl)
+        correct_train = 0
+        total_train = 0
+        loss_train = 0
+
+        for batch_num, (motion_batch, label_batch) in enumerate(train_dl):
+            batch_sz = len(motion_batch)
+            label_batch = label_batch.to(DEVICE)
+            motion_batch = motion_batch.to(DEVICE)
+            output = model(motion_batch)
+            loss_train = nn.CrossEntropyLoss()(output, label_batch)
+
+            optimizer.zero_grad()
+            loss_train.backward()
+            optimizer.step()
+
+            preds_train = torch.argmax(output, dim=1)
+            correct_train += int(torch.eq(preds_train, label_batch).sum())
+            total_train += batch_sz
+            minibatch_accuracy_train = 100 * correct_train / total_train
+
+            #### Fancy printing stuff, you can ignore this! ######
+            if (batch_num + 1) % 5 == 0:
+                print(" " * len(msg), end='\r')
+                msg = f'Train epoch[{epoch+1}/{epochs}], MiniBatch[{batch_num + 1}/{total_steps_train}], Loss: {loss_train.item():.5f}, Acc: {minibatch_accuracy_train:.5f}, LR: {lr_scheduler.get_last_lr()[0]:.5f}'
+                print (msg, end='\r' if epoch < epochs else "\n",flush=True)
+            #### Fancy printing stuff, you can ignore this! ######
+                
+        lr_scheduler.step()
+
+        ########################################################################
+        print("") # Create newline between progress bars
+        #######################VALIDATION STEP##################################
+
+        model.eval()
+        val_dl = dataloaders['val']
+
+        total_steps_val = len(val_dl)
+        correct_val = 0
+        total_val = 0
+        loss_val = 0
+
+        for batch_num, (motion_batch,label_batch) in enumerate(val_dl):
+            batch_sz = len(motion_batch)
+            motion_batch = motion_batch.to(DEVICE)
+            label_batch = label_batch.to(DEVICE)
+
+            with torch.no_grad():
+                output = model(motion_batch)
+                loss_val = nn.CrossEntropyLoss()(output, label_batch)
+
+                preds_val = torch.argmax(output, dim = 1)
+
+                correct_val += int(torch.eq(preds_val, label_batch).sum())
+                total_val += batch_sz
+                minibatch_accuracy_val = 100 * correct_val / total_val
+
+                #### Fancy printing stuff, you can ignore this! ######
+                if (batch_num + 1) % 4 == 0:
+                    print(" " * len(msg), end='\r')
+                    msg = f'Eval epoch[{epoch+1}/{epochs}], MiniBatch[{batch_num + 1}/{total_steps_val}], Loss: {loss_val.item():.5f}, Acc: {minibatch_accuracy_val:.5f}'
+                    if early_stopper.epoch_counter > 0:
+                        msg += f", Epochs without improvement: {early_stopper.epoch_counter}"
+                    print (msg, end='\r' if epoch < epochs else "\n",flush=True)
+                #### Fancy printing stuff, you can ignore this! ######
+                    
+        ########################################################################
+        print("")  # Create newline between progress bars
+        ########################################################################
+
+        epoch_train_acc = 100 * correct_train / total_steps_train
+        epoch_val_acc = 100 * correct_val/ total_steps_val
+        writer.add_scalar("loss/train",loss_train,epoch)
+        writer.add_scalar("loss/train",loss_val,epoch)
+        writer.add_scalar("Acc/train",epoch_train_acc,epoch)
+        writer.add_scalar("Acc/val", epoch_val_acc,epoch)
+
+        if epoch % checkpoint_frequency == 0:
+            save_checkpoint(model, epoch, "./saved_models")
+        if early_stopper.should_stop(epoch_val_acc):
+            print(f"\nValidation accuracy has not improved in {early_stopper.epoch_counter} epochs, stopping.")
+            save_checkpoint(model,epoch,"./saved_models")
+            return
